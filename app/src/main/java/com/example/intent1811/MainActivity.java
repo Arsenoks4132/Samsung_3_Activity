05526844a42776589670ee2f7fdcd3cb947670c6
Arsenoks4132 <Danov2005@yandex.ru>
2023-11-18 11:12:32 +0300

    Laptop

---
+++ app/src/main/java/com/example/intent1811/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.intent1811;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.intent1811.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        binding.GO.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        setContentView(R.layout.activity_main);
+    }
+}
